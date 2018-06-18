65c807e0067545fdd232cbcc58d3ab7edd303993
Christopher Iossa <chris.iossa@gmail.com>
2018-06-18 12:05:16 -0400

    Refined Communication to work over wifi (Server changes)

--- Source/Version 1.1.2/server.py
+++ Source/Version 1.1.2/server.py
@@ -1,18 +1,25 @@
 import socket               # Import socket module
 import datetime
 import json
+import zlib
+import hashlib
 from Marker import Marker
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
-host = '169.254.114.139'    # Get local machine name
-print(host)
-port = 54321                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+host = '192.168.0.100'    # Get local machine name
+port = 12345                # Reserve a port for your service.
+cameraCount = 1
+print(host)
+s.bind((host, port))        # Bind to the port
 s.listen(5)                 # Now wait for client connection.
-c, addr = s.accept()        # Establish connection with client.
-print('Got connection from')
-print(addr)
+connections = {}
+for i in range (0, cameraCount):
+    c, addr = s.accept()        # Establish connection with client.
+    connections [addr] = c
+for k,v in connections.iteritems():
+    print('Got connection from')
+    print(k)
 cont = True
 markerList = []
 while cont: #while the client has not sent the Disconnect signal
@@ -21,29 +28,37 @@ while cont: #while the client has not sent the Disconnect signal
         cont = False
         print(msg)
     elif (msg=="Dumping"):
-        c.send("Ready")
+        c.send("OK")
+        theirHash=c.recv(40)
+        print theirHash
+        myHash = hashlib.sha1()
         stillRecv = True
+        jsonFile =""
         while (stillRecv):
-            msg=c.recv(4096)
-            msg = msg.strip("\n ' '") 
-            if(msg[0]=='{'):
+            chunk=c.recv(1024)
+            jsonFile += chunk
+            if(jsonFile[-1]==chr(4)):
+                stillRecv=False
+        jsonFile = jsonFile.strip("\n ' '")
+        print "JSON File: " +jsonFile
+        records = jsonFile.split('\n') #turn line delimited JSON records into list of JSON records      
+        for i in records:
+            if(i[0] != '{'):
+                print i
+            if (i[-1] != '}'):
+                print i
+            else:
                 temp=Marker(0,0,'NA')
-                msg = msg.strip("\n ' '") #trim trailing whitespace
-                #records = msg.split('\n') #turn line delimited JSON records into list of JSON records      
-                markerList.append(temp.jsonLoad(msg))
-                print markerList[-1].jsonDump()
-                c.send(str(markerList[-1].GUID))
-                msg = c.recv(4096)
-                if (msg=="OK"):
-                    #print msg
-                    c.send("Ready")
-            elif(msg=="Done"):
-                    c.send(str(len(markerList)))
-                    msg = c.recv(4096)
-                    if (msg == "Equal"):
-                        print msg;
-                        stillRecv=False
-                
+                markerList.append(temp.jsonLoad(i))
+                myHash.update(str(temp.GUID))
+                print(markerList[-1].jsonDump())
+        print ("My Hash {0}".format(myHash.hexdigest()))
+        print ("Their Hash {0}".format(theirHash))
+        if(myHash.hexdigest() == theirHash):
+            print ("Match")
+        else:
+            print ("Mismatch, resend")
+            markerList = []
                 
                     
                     
