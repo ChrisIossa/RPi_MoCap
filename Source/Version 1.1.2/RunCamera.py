a36564a85548cafce0f01524d7e6138b838f4611
Matheus Alexandre <matheusrochalex@hotmail.com>
2018-06-22 12:31:54 -0400

    Code Block replacement
    Replaced the location of the parameter parsing so that it flows with the camera capture

--- Source/Version 1.1.2/RunCamera.py
+++ Source/Version 1.1.2/RunCamera.py
@@ -42,6 +42,50 @@ if validImports:
 
         print("Attempting to start camera capture feed...")
         vid.startCapture()
+                    # Command Line Parameters
+
+            parser = argparse.ArgumentParser()
+            parser.add_argument('integers', type=int, choices = range(255), help='value for the threshold')
+            parser.add_argument('ipaddress', help='ip address of the camera')
+            parser.add_argument('port', type=int, help = 'the port number')
+            parser.add_argument('label', help = 'the name/label of the camera')
+            parser.add_argument('frame_freq', type=int, help='the frequency of frames to be sent')
+            parser.add_argument('-s', '--send', help='determine if frequency of frames is displayed or not', action='store_true')
+            parser.add_argument('infile', type=argparse.FileType('r'), help = 'the file to operate on')
+
+            #parse the command
+            args = parser.parse_args()
+
+            #check if the command only has one argument
+            if len(sys.argv) == 1:
+                #if so, check if the argument is an xml file
+                try:
+                    tree = ET.parse(args.infile)
+                    root = tree.getroot()
+                except:
+                    print("Argument is not an XML file")
+            else:
+            #assigning values
+                thresholdValue = args.integers
+                temp = args.ipaddress
+                temp = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$") #ip address input validation
+                #check if the ip address matches the standard "000.000.000.000" format
+                if temp:
+                    ipAddress = re.group()
+                port = args.port
+                label = args.label
+                frames = args.frame_freq
+    
+            #printing values
+            print("Values given are: ")
+            print("Threshold -- " + thresholdValue)
+            print("IP Address -- " + ipAddress)
+            print("Port Number -- " + port)
+            print("Label Name -- " + label)
+            #if the -s argument is set then show the frame frequency
+            if args.send:
+                print("Frame Frequency -- " + frames)
+    
         '''
         try:
             vid.startCapture()
@@ -50,56 +94,13 @@ if validImports:
             cameraCaptureStarted = False
         else:
             cameraCaptureStarted = True
-            '''
+        '''
 
 # Otherwise, don't bother starting up camera
 else:
     print("Could not load modules. Aborting camera operation")
 
-# Command Line Parameters
 
-parser = argparse.ArgumentParser()
-parser.add_argument('integers', type=int, choices = range(255), help='value for the threshold')
-parser.add_argument('ipaddress', help='ip address of the camera')
-parser.add_argument('port', type=int, help = 'the port number')
-parser.add_argument('label', help = 'the name/label of the camera')
-parser.add_argument('frame_freq', type=int, help='the frequency of frames to be sent')
-parser.add_argument('-s', '--send', help='determine if frequency of frames is displayed or not', action='store_true')
-parser.add_argument('infile', type=argparse.FileType('r'), help = 'the file to operate on')
-
-#parse the command
-args = parser.parse_args()
-
-#check if the command only has one argument
-if len(sys.argv) == 1:
-    #if so, check if the argument is an xml file
-    try:
-        tree = ET.parse(args.infile)
-        root = tree.getroot()
-    except:
-        print("Argument is not an XML file")
-else:
-    #assigning values
-    thresholdValue = args.integers
-    temp = args.ipaddress
-    temp = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$") #ip address input validation
-    #check if the ip address matches the standard "000.000.000.000" format
-    if temp:
-        ipAddress = re.group()
-    port = args.port
-    label = args.label
-    frames = args.frame_freq
-    
-#printing values
-print("Values given are: ")
-print("Threshold -- " + thresholdValue)
-print("IP Address -- " + ipAddress)
-print("Port Number -- " + port)
-print("Label Name -- " + label)
-#if the -s argument is set then show the frame frequency
-if args.send:
-    print("Frame Frequency -- " + frames)
-    
 
 # Marker testing
 '''
